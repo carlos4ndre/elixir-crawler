7a3be9fbd35a51a54d09eb2cbbfdfe5bc8a50429
Carlos Andre <carlos.ulrich@gmail.com>
2017-07-28 17:38:20 +0100

    Update sitemap tests

--- apps/sitemap/test/sitemap_test.exs
+++ apps/sitemap/test/sitemap_test.exs
@@ -18,20 +18,4 @@ defmodule SitemapTest do
     assert SiteMap.has_url?(url) == false
   end
 
-  @tag :capture_log
-  test "pages with invalid url are not added to sitemap" do
-    invalid_pages = [
-      %Page{url: ""},
-      %Page{url: "."},
-      %Page{url: ".com"},
-      %Page{url: "example."},
-      %Page{url: "example.com"},
-      %Page{url: "dog://example.com"},
-    ]
-
-    for page <- invalid_pages do
-      SiteMap.add_page(page)
-      assert SiteMap.has_url?(page.url) == false
-    end
-  end
 end
