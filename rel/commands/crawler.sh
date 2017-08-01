99eed0972864aec124213f0aae2b79586ad970ee
Carlos André <carlos.ulrich@gmail.com>
2017-08-01 22:18:43 +0100

    Parse argv when value is scalar

--- rel/commands/crawler.sh
+++ rel/commands/crawler.sh
@@ -5,5 +5,6 @@ bin/elixir_crawler start
 sleep 5
 
 echo "Running elixir crawler"
-bin/elixir_crawler rpc Elixir.CLI.Runner main "$@"
+shift
+bin/elixir_crawler rpc Elixir.CLI.Runner main $@
 echo "Done!"
